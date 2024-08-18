283d021407c2fa1edaf88edea2211b8fa1a3bb9a
ammar <ammarbashirharuna2020@gmail.com>
2024-08-18 19:55:22 +0100

    deleting code for function file

---
+++ List.py
@@ -0,0 +1,15 @@
+#python list
+list_of_items = ["mango","banana","pineApple", "pawpaw", "cashow", "gwava", "pese",]
+#finding leng of the list
+total_leng = len(list_of_items)
+print(total_leng)
+#adding another fruit to the list
+list_of_items.append("groundnut")
+list_of_items.append("coconut")
+list_of_items.append("date")
+print(list_of_items)
+#to search for a specific item in the list
+item = list_of_items[3]
+print(item)
+#to replace a specific item in the list
+new_bana = list_of_items[1] = "apple"
--- function.py
+++ function.py
@@ -1,35 +1,3 @@
-from math import remainder
-from random import choice
-def calculate(firstnum, operator,secondnum):
-    if operator == '+':
-        sum = firstnum + secondnum
-        return sum
-    elif operator == '*':
-        multiply = firstnum * secondnum
-        return multiply
-    elif operator == '-':
-        subtract = firstnum - secondnum
-        return subtract
-    elif operator == '/':
-        divide = firstnum / secondnum
-        return divide
-    elif operator == '%':
-        remainder = firstnum % secondnum
-        return remainder
-    else :
-        return "invalid value"
-print("welcome to our calculator".upper())
-print("[1]addition\n[2]multiplication\n[3]subtraction\n[4]divide\n[5]reminder".title())
-choose = input("enter your choice")
-to_int = int(choose)
-if to_int == 1:
-    print("welcome to add wizard".title())
-    num = input("enter first NO. ".title())
-    to_int = int(num)
-    num2 = input("Enter second NO. ".title())
-    to_integer = int(num2)
-    print(f"the sum of your numbers is {calculate(to_int, '+',to_integer)}")
-    
-    
-
-
+def calculate(a,b):
+    add = a+ b
+    return add
